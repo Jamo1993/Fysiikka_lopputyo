88aeaff2dcb729c4ebe663e64a6c46b0679d30ed
Jamo <jamo.jaakola@gmail.com>
2025-12-18 09:50:46 +0200

    url korjaus

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ from streamlit_folium import st_folium
 from scipy.signal import butter, filtfilt
 from math import radians, cos, sin, asin, sqrt
 
-url = "https://raw.githubusercontent.com/Jamo1993/Fysiikka_lopputyo/refs/heads/main/app.py"
+ACC_URL = "https://raw.githubusercontent.com/Jamo1993/Fysiikka_lopputyo/main/Acceleration2.csv"
+LOC_URL = "https://raw.githubusercontent.com/Jamo1993/Fysiikka_lopputyo/main/Location2.csv"
+
+df_acc = pd.read_csv(ACC_URL)
+df_loc = pd.read_csv(LOC_URL)
+
 st.title("Spurttivartti")
 
 def butter_lowpass_filter(data, cutoff, nyq, order):
